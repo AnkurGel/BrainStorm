65ae12cec41b5314a2d93a56d28b8ac505bc46aa
Ankur Goel <ankurgel@gmail.com>
2013-03-13 00:08:37 +0530

    removed debugger command

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,7 +28,6 @@ class User < ActiveRecord::Base
 
     user = where(:provider => auth.provider, :uid => auth.uid)
     new_record = true if user.empty?
-    debugger
     new_user = where(auth.slice(:provider, :uid)).first_or_initialize.tap do |user|
       user.provider = auth.provider
       user.uid = auth.uid
